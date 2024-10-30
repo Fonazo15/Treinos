3a220a4313c6b2dada91ff1ff3d809aa76a09613
Fonazo15 <felipeantoniodesouzaesilva@gmail.com>
2024-10-30 08:39:22 -0300

    Treino 1

---
+++ Assets/Scripts/CollBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollBehaviour : MonoBehaviour
+{
+    public enum CollType
+    {
+        SpeedColl, ColorColl, SceneColl
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    [Header("Events")]
+    public UnityEvent onGameEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
